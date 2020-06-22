8abc320cdfaf0188b9ef2314d3701aa1a7bdf851
Sherwyn Dsouza <sherwyndsouza1999@gmail.com>
2020-06-22 19:02:02 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -3,4 +3,4 @@ YOLOV3_CFG_PATH = './yolov3/yolov3.cfg'
 YOLOV3_WEIGHTS_PATH = './yolov3/yolov3.weights'
 VIDEO_PATH = './videos/video.mp4'
 OUTPUT_PATH = './output/output.avi'
-SAFE_DISTANCE = 61
+SAFE_DISTANCE = 60
